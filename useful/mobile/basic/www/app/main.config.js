057daee39971d7732a1c8d0393d3b4906bb0887c
Diego Silva <dsilva2401@gmail.com>
2015-05-21 21:55:21 -0500

    Bug fixed

--- useful/mobile/basic/www/app/main.config.js
+++
@@ -1,9 +0,0 @@
-(function(ang) {
-
-	var app = ang.module('app');
-
-	app.config( ['$mdThemingProvider', function( $mdThemingProvider ) {
-		$mdThemingProvider.theme('default').primaryPalette('blue');
-	}]);
-
-})(angular)
